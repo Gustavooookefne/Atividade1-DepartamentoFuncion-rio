bad3d39e71d146ff99dd1d23515254565e35af4c
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-20 19:35:30 -0300

    feat: Finalizado o Funcionario

--- DepartamentoEFuncionário/DepartamentoEFuncionário/src/main/java/com/Weg/DepartamentoEFuncionario/model/Funcionario.java
+++ DepartamentoEFuncionário/DepartamentoEFuncionário/src/main/java/com/Weg/DepartamentoEFuncionario/model/Funcionario.java
@@ -1,4 +1,36 @@
 package com.Weg.DepartamentoEFuncionario.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome_funcionario" , nullable = false)
+    private String nome;
+
+    @Column(name = "salario",nullable = false)
+    private double salario;
+
+    @Column(name = "cargo", nullable = false)
+    private String cargo;
+
+    public Funcionario(String nome, double salario, String cargo) {
+        this.nome = nome;
+        this.salario = salario;
+        this.cargo = cargo;
+    }
+
+
 }
