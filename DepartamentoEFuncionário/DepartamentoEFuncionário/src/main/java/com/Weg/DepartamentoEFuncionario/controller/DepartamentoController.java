a79dd2283331a287082df9b418c4bcb607bd5a91
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-20 20:41:49 -0300

    feat: Finalizado o FuncionarioController

--- DepartamentoEFuncionário/DepartamentoEFuncionário/src/main/java/com/Weg/DepartamentoEFuncionario/controller/DepartamentoController.java
+++ DepartamentoEFuncionário/DepartamentoEFuncionário/src/main/java/com/Weg/DepartamentoEFuncionario/controller/DepartamentoController.java
@@ -1,4 +1,9 @@
 package com.Weg.DepartamentoEFuncionario.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/departamento")
 public class DepartamentoController {
 }
--- DepartamentoEFuncionário/DepartamentoEFuncionário/src/main/java/com/Weg/DepartamentoEFuncionario/controller/FuncionarioController.java
+++ DepartamentoEFuncionário/DepartamentoEFuncionário/src/main/java/com/Weg/DepartamentoEFuncionario/controller/FuncionarioController.java
@@ -1,4 +1,46 @@
 package com.Weg.DepartamentoEFuncionario.controller;
 
+import com.Weg.DepartamentoEFuncionario.dto.FuncionarioDto.FuncionarioRequestDto;
+import com.Weg.DepartamentoEFuncionario.dto.FuncionarioDto.FuncionarioResponseDto;
+import com.Weg.DepartamentoEFuncionario.model.Funcionario;
+import com.Weg.DepartamentoEFuncionario.service.FuncionarioService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/funcionario")
 public class FuncionarioController {
+
+    private final FuncionarioService service;
+
+    @PutMapping
+    public FuncionarioResponseDto salvar (@RequestBody FuncionarioRequestDto requestDto){
+
+        return service.salvar(requestDto);
+    }
+    @GetMapping
+    public List<FuncionarioResponseDto> listarTodos(){
+
+        return service.listarTodos();
+    }
+    @GetMapping("/{id}")
+    public FuncionarioResponseDto listarPorId (UUID id){
+
+        return service.listarPorId(id);
+    }
+    @PostMapping("/{id}")
+    public FuncionarioResponseDto atulizar (FuncionarioRequestDto requestDto, UUID id){
+
+        return service.atualizar(requestDto , id);
+    }
+    @DeleteMapping("/{id}")
+    public void deletar (UUID id){
+
+        service.deletar(id);
+    }
+
 }
