1ab99ff733a3683e5828acb428c3b8839de145e9
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-20 19:37:23 -0300

    feat: Finalizado o Departamento

--- DepartamentoEFuncionário/DepartamentoEFuncionário/src/main/java/com/Weg/DepartamentoEFuncionario/model/Departamento.java
+++ DepartamentoEFuncionário/DepartamentoEFuncionário/src/main/java/com/Weg/DepartamentoEFuncionario/model/Departamento.java
@@ -1,4 +1,21 @@
 package com.Weg.DepartamentoEFuncionario.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_departamento", nullable = false)
+    private String nomeDoDepartamento;
 }
