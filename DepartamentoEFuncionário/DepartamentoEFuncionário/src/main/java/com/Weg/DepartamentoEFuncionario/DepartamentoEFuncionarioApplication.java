c7a3c7acac8f5ed5f6475c4da8b91f97ce97e83c
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-20 19:27:27 -0300

    Add files via upload

---
+++ DepartamentoEFuncionário/DepartamentoEFuncionário/src/main/java/com/Weg/DepartamentoEFuncionario/DepartamentoEFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package com.Weg.DepartamentoEFuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartamentoEFuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartamentoEFuncionarioApplication.class, args);
+	}
+
+}
---
+++ DepartamentoEFuncionário/DepartamentoEFuncionário/src/test/java/com/Weg/DepartamentoEFuncionario/DepartamentoEFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Weg.DepartamentoEFuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartamentoEFuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
